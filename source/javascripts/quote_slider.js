1a23ef66e40518918047fb76fa1b39cbac31085c
Jakob Skjerning <jakob@mentalized.net>
2013-03-19 13:33:44 +0100

    Less obvious copy/paste job

--- source/javascripts/quote_slider.js
+++ source/javascripts/quote_slider.js
@@ -61,12 +61,12 @@
     };
 
     // Method calling logic
-     if ( methods[method] ) {
-       return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-     } else if ( typeof method === 'object' || ! method ) {
-       return methods.init.apply( this, arguments );
+     if (methods[method]) {
+       return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+     } else if (typeof method === 'object' || ! method) {
+       return methods.init.apply(this, arguments);
      } else {
-       $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+       $.error('Method ' +  method + ' does not exist on jQuery.quoteSlider');
      };
 
 

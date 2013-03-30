1d402e202e55859966d47c7338a44a42df07c237
Jakob Skjerning <jakob@mentalized.net>
2013-03-30 11:57:58 +0100

    Remove unused, delmented code

--- config.rb
+++ config.rb
@@ -1,57 +1,12 @@
 require 'slim'
-
-###
-# Compass
-###
-
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
 
 # Add asset hash to URLs
 activate :asset_hash
 
+# Deployment
 LOCALE = ENV["LOCALE"]
 if LOCALE
   # Deploy to UnoEuro
@@ -76,10 +31,6 @@ activate :i18n, :mount_at_root => ENV["LOCALE"] || :en
 require 'lib/i18n_helpers'
 helpers I18nHelpers
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-
 require 'lib/asset_helpers'
 helpers AssetHelpers
 
@@ -100,18 +51,4 @@ configure :build do
 
   # Minify Javascript on build
   activate :minify_javascript
-
-  # Enable cache buster
-  # activate :cache_buster
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
 end

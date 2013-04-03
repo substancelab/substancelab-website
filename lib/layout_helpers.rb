bc51a3d76bb0690cc6a332e00bec9092af8655d0
Jakob Skjerning <jakob@mentalized.net>
2013-04-03 11:54:14 +0200

    Tweak page titles

--- lib/layout_helpers.rb
+++ lib/layout_helpers.rb
@@ -8,4 +8,18 @@ module LayoutHelpers
     attrs
   end
 
+  def page_title
+    title = if content_for?(:page_title)
+      yield_content :page_title
+    else
+      current_page.data.title
+    end
+
+    if title.blank?
+      "Substance Lab - Carefully crafted web applications and websites"
+    else
+      "#{title} | Substance Lab"
+    end
+  end
+
 end

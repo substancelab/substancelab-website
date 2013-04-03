b6d2237ca1e0a8ac9cf4d5a4d7c94b7fb75f5c39
Jakob Skjerning <jakob@mentalized.net>
2013-04-03 09:28:39 +0200

    Add Google Analytics back in, oops

--- lib/i18n_helpers.rb
+++ lib/i18n_helpers.rb
@@ -6,8 +6,10 @@ module I18nHelpers
 
   # If partial exists for the current locale, that partial is rendered
   def localized_partial(partial_path)
-    partial(File.join(I18n.locale.to_s, partial_path))
+    full_path = File.join(I18n.locale.to_s, partial_path)
+    partial(full_path)
   rescue Middleman::CoreExtensions::Rendering::TemplateNotFound
+    puts "#{partial_path.inspect} not found for locale #{I18n.locale.to_s} at #{full_path.inspect}. Falling back to #{fallback_locale.inspect}"
     # The partial doesn't exist for current locale, fall back to fallback locale
     partial(File.join(fallback_locale, partial_path))
   end

a5aa7eab06c51ee504fefc023b53e5ff7182d9ce
Jakob Skjerning <jakob@mentalized.net>
2014-04-22 11:58:27 +0200

    Pluginify adjustEqualHeights

--- source/javascripts/ensure_equal_heights.js
+++ source/javascripts/ensure_equal_heights.js
@@ -1,17 +1,45 @@
 //= require jquery.equalheights.min.js
 
-function adjustEqualHeights(selectorsToAdjust) {
- 
-  $(document).ready(function() {
-    adjustOnAction(selectorsToAdjust);
-  });
-  $( window ).resize(function() {
-    adjustOnAction(selectorsToAdjust);
-  });
-}
+(function( $ ) {
+  $.fn.adjustEqualHeights = function(method) {
+    var methods = {
+      init: function(options) {
+        var $this = $(this);
+        $this.adjustEqualHeights('adjustEqualHeights');
+      },
 
-function adjustOnAction(contentToAdjust) {
-   for(var i=0; i < contentToAdjust.length; i++) {
-    $(contentToAdjust[i]).css({height: ""}).equalHeights();
-  }
-}
+      adjustEqualHeights: function() {
+        var $this = $(this);
+
+        $(document).ready(function() {
+          $this.adjustEqualHeights('adjustOnAction');
+        });
+        $(window).resize(function() {
+          $this.adjustEqualHeights('adjustOnAction');
+        });
+      },
+
+      adjustOnAction: function() {
+        var $this = $(this);
+
+        $this.each(function(index, element) {
+          $(element).css({height: ""}).equalHeights();
+        });
+      }
+    };
+
+    // Method calling logic
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || ! method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method ' +  method + ' does not exist on jQuery.adjustEqualHeights');
+    };
+
+    // Return this to ensure chainability
+    return this.each(function() {
+      var $this = $(this);
+    });
+  };
+})( jQuery );

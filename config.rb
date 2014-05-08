cb915ba2e193a0afca81022793f28318c8110dcc
Jakob Skjerning <jakob@mentalized.net>
2014-05-08 13:21:52 +0200

    Place favicons inside images folder

--- config.rb
+++ config.rb
@@ -79,16 +79,16 @@ page "/sitemap.xml", :layout => false
 # Build-specific configuration
 configure :build do
   activate :favicon_maker, :icons => {
-    "favicon_template.png" =>   [
-      { icon: "apple-touch-icon-152x152-precomposed.png" },
-      { icon: "apple-touch-icon-144x144-precomposed.png" },
-      { icon: "apple-touch-icon-114x114-precomposed.png" },
-      { icon: "apple-touch-icon-72x72-precomposed.png" },
-      { icon: "apple-touch-icon-precomposed.png", size: "57x57" },
-      { icon: "apple-touch-icon.png", size: "57x57" },
-      { icon: "favicon.png", size: "16x16" },
-      { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
-      { icon: "mstile-144x144", format: "png" },
+    "/images/favicon_template.png" =>   [
+      { icon: "/images/apple-touch-icon-152x152-precomposed.png" },
+      { icon: "/images/apple-touch-icon-144x144-precomposed.png" },
+      { icon: "/images/apple-touch-icon-114x114-precomposed.png" },
+      { icon: "/images/apple-touch-icon-72x72-precomposed.png" },
+      { icon: "/images/apple-touch-icon-precomposed.png", size: "57x57" },
+      { icon: "/images/apple-touch-icon.png", size: "57x57" },
+      { icon: "/images/favicon.png", size: "16x16" },
+      { icon: "/images/favicon.ico", size: "64x64,32x32,24x24,16x16" },
+      { icon: "/images/mstile-144x144", format: "png" },
     ]
   }
 

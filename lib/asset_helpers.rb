447231ab419225202a1c8838ee7fbf5f2c32b6ea
Jakob Skjerning <jakob@mentalized.net>
2017-02-28 09:20:41 +0100

    Encapsulate building absolute URLs

--- lib/asset_helpers.rb
+++ lib/asset_helpers.rb
@@ -2,4 +2,14 @@ module AssetHelpers
   def image_exists?(path)
     File.exist?(File.join(source, images_dir, path))
   end
+
+  # Returns an absolute URL to the specified image
+  def image_url(image)
+    path = image_path(image)
+    return if path.start_with?("http")
+    port = config[:port]
+    hostname = config[:host]
+    hostname = "#{hostname}:#{port}" if port && port.to_i != 80
+    "http://#{hostname}#{path}"
+  end
 end

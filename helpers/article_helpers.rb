a1bf88829234486eb23d1b3a389745a9d76f2cf0
Jakob Skjerning <jakob@mentalized.net>
2017-03-24 16:05:37 +0100

    Articles now know their own URL

--- helpers/article_helpers.rb
+++ helpers/article_helpers.rb
@@ -25,25 +25,12 @@ module ArticleHelpers
     File.join("articles")
   end
 
-  # Returns the full path to article
-  def article_path(article)
-    permalink = blog.options.permalink
-    article_date = article.date
-
-    # This snippet is copied from blog_data.rb in blog-middleman
-    permalink.
-      sub(":year", article_date.year.to_s).
-      sub(":month", article_date.month.to_s.rjust(2, "0")).
-      sub(":day", article_date.day.to_s.rjust(2, "0")).
-      sub(":title", article.slug)
-  end
-
   def articles_path
     blog.options.prefix
   end
 
   def link_to_article(article)
-    href = article_path(article)
+    href = article.url
     title = article.title
     if block_given?
       link_to(href, :title => title, &block)

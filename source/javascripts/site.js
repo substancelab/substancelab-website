de096abc48de08e970f36287e52cd7b3670bf652
Jakob Skjerning <jakob@mentalized.net>
2017-11-11 21:48:09 +0100

    Let's not rely on Javascript for setting equal heights

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,8 +1,6 @@
 import 'quote_slider'
-// require 'ensure_equal_heights.js'
 
 window.setupHomepage = function() {
   console.debug('Setup homepage')
   $('.sliding_quotes').quoteSlider();
-  // $(['.featured-work .tile', '.article-list .tile']).adjustEqualHeights();
 }

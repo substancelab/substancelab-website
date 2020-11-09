9e032ba30875b9993afcde3e858bdb37efb96b49
Jakob Skjerning <jakob@mentalized.net>
2020-11-09 08:57:31 +0100

    We're on whatever we've configured

--- helpers/asset_helpers.rb
+++ helpers/asset_helpers.rb
@@ -9,9 +9,11 @@ module AssetHelpers
   def image_url(image)
     path = image_path(image)
     return if path.start_with?("http")
+
+    protocol = config[:protocol]
     port = config[:port]
     hostname = config[:host]
     hostname = "#{hostname}:#{port}" if port && port.to_i != 80
-    "https://#{hostname}#{path}"
+    "#{protocol}#{hostname}#{path}"
   end
 end

4e0cbff3c4f313b5940e0d6aff8982512ea0e822
Jakob Skjerning <jakob@mentalized.net>
2026-06-15 15:57:59 +0200

    Start styling all over

--- frontend/entrypoints/site.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery'
-import '../quote_slider'
-
-$(function() {
-  if ($('.sliding_quotes').length) {
-    $('.hero').addClass('peptalk-is-visible');
-    $('.sliding_quotes').quoteSlider();
-  }
-})
